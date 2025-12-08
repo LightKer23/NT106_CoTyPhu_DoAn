7087a8eea4a210b87302a5aace279242dfcb93dc
LightKer23 <hauvq0468@gmail.com>
2025-12-08 17:14:53 +0700

    feat: design MenuForm

--- CoTyPhu/Client/Views/Forms/MenuForm.Designer.cs
+++ CoTyPhu/Client/Views/Forms/MenuForm.Designer.cs
@@ -29,22 +29,83 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            btnPlayWithCmp = new Button();
+            btnPlayWithPlayer = new Button();
+            btnGuide = new Button();
+            btnSetting = new Button();
+            btnExit = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(428, 74);
+            label1.Font = new Font("Tahoma", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(302, 44);
             label1.Name = "label1";
-            label1.Size = new Size(85, 20);
+            label1.Size = new Size(177, 34);
             label1.TabIndex = 0;
-            label1.Text = "NOEL-POLY";
+            label1.Text = "NOEL - POLY";
+            // 
+            // btnPlayWithCmp
+            // 
+            btnPlayWithCmp.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPlayWithCmp.Location = new Point(315, 163);
+            btnPlayWithCmp.Name = "btnPlayWithCmp";
+            btnPlayWithCmp.Size = new Size(150, 50);
+            btnPlayWithCmp.TabIndex = 1;
+            btnPlayWithCmp.Text = "Chơi với máy";
+            btnPlayWithCmp.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayWithPlayer
+            // 
+            btnPlayWithPlayer.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPlayWithPlayer.Location = new Point(315, 107);
+            btnPlayWithPlayer.Name = "btnPlayWithPlayer";
+            btnPlayWithPlayer.Size = new Size(150, 50);
+            btnPlayWithPlayer.TabIndex = 2;
+            btnPlayWithPlayer.Text = "Chơi với người";
+            btnPlayWithPlayer.UseVisualStyleBackColor = true;
+            // 
+            // btnGuide
+            // 
+            btnGuide.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGuide.Location = new Point(315, 219);
+            btnGuide.Name = "btnGuide";
+            btnGuide.Size = new Size(150, 50);
+            btnGuide.TabIndex = 3;
+            btnGuide.Text = "Hướng dẫn chơi";
+            btnGuide.UseVisualStyleBackColor = true;
+            // 
+            // btnSetting
+            // 
+            btnSetting.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSetting.Location = new Point(315, 275);
+            btnSetting.Name = "btnSetting";
+            btnSetting.Size = new Size(150, 50);
+            btnSetting.TabIndex = 4;
+            btnSetting.Text = "Cài đặt";
+            btnSetting.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExit.Location = new Point(315, 331);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(150, 50);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = true;
             // 
             // MenuForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 453);
+            ClientSize = new Size(782, 403);
+            Controls.Add(btnExit);
+            Controls.Add(btnSetting);
+            Controls.Add(btnGuide);
+            Controls.Add(btnPlayWithPlayer);
+            Controls.Add(btnPlayWithCmp);
             Controls.Add(label1);
             Name = "MenuForm";
             ShowIcon = false;
@@ -56,5 +117,10 @@
         #endregion
 
         private Label label1;
+        private Button btnPlayWithCmp;
+        private Button btnPlayWithPlayer;
+        private Button btnGuide;
+        private Button btnSetting;
+        private Button btnExit;
     }
 }
