5e4d9f0e6372a58bcd116c4d63a7f470341963fa
LightKer23 <hauvq0468@gmail.com>
2025-12-15 13:23:26 +0700

    feat: sketch HowToPlayForm

--- CoTyPhu/Client/Views/Forms/HowToPlayForm.Designer.cs
+++ CoTyPhu/Client/Views/Forms/HowToPlayForm.Designer.cs
@@ -28,19 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            richTextBox1 = new RichTextBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.Location = new Point(132, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(234, 32);
+            label1.TabIndex = 0;
+            label1.Text = "HƯỚNG DẪN CHƠI";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(32, 104);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(446, 334);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
             // 
             // HowToPlayForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(505, 470);
+            Controls.Add(richTextBox1);
+            Controls.Add(label1);
             Name = "HowToPlayForm";
             ShowIcon = false;
             Text = "Hướng dẫn chơi";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private RichTextBox richTextBox1;
     }
 }
