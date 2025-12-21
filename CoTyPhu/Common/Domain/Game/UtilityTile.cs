1f7252b4ed16efe759a0886244231507b8f24e28
anhhao1712 <bahao171206@gmail.com>
2025-12-21 23:54:50 +0700

    fix: delete common-player

--- CoTyPhu/Common/Domain/Game/UtilityTile.cs
+++ CoTyPhu/Common/Domain/Game/UtilityTile.cs
@@ -2,7 +2,7 @@
 public class UtilityTile : Tile
 {
     public int buyPrice { get; set; }
-    public Player? Owner { get; set; } = null;
+    public int? Owner { get; set; } = null;
     public int sellPrice { get; set; }
 
     private Dice dice = new Dice();
